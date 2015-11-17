2073bc337639c5bb1407e37fd6c4fe99c518614c
flappymonkey <shenbin1988@gmail.com>
2015-11-17 22:44:34 +0800

    初始化餐饮秀网站

---
+++ demos/coffee/data/total.js
@@ -0,0 +1,1 @@
+var totalmanifest=350;
---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+$(function(){
+	$('#banner').bjqs({
+          'animation' : 'slide',
+          'width' : 960,
+          'height' : 400,
+		  'showMarkers' : false,
+		  'centerControls' :true,
+		  'nextText': '<i class="icon-chevron-right icon-white"></i>',
+		  'prevText': '<i class="icon-chevron-left icon-white"></i>',
+		  'useCaptions' : false,
+		  'keyboardNav' : true
+        });
+});
